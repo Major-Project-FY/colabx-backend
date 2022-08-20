55e9f75fa069ef4159692fc2e5e1affb1e41f3e2
Mayuresh-loves-cpp <mvshinde640@gmail.com>
2022-08-20 08:47:02 +0530

    backend: fixing routes

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,17 @@
 import express, { Application, Request, Response } from "express";
-// import "dotenv";
+import "dotenv/config";
 
 import userRouter from "./routes/user.routes";
 
-const port: number = 5000;
+const port: any = process.env.PORT || 5000;
 
 const app: Application = express();
 
 app.get("/", (req: any, res: Response) => {
   res.send("hello!");
 });
+
+app.use("/route", userRouter);
 
 app.listen(port, () => {
   console.log(`server started on port ${port}`);
